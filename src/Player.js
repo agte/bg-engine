d395feeb51bd167a222fed677f554a78d48c01a6
Anna Agte <matiasrustagte@gmail.com>
2019-10-01 16:20:37 +0500

    fix: remove property active from Player class

--- src/Player.js
+++ src/Player.js
@@ -3,8 +3,6 @@ import type from '@agte/type';
 export default class Player {
   #score;
 
-  #active;
-
   get score() {
     return this.#score;
   }
@@ -14,23 +12,13 @@ export default class Player {
     this.#score = value;
   }
 
-  get active() {
-    return this.#active;
-  }
-
-  set active(value) {
-    type.boolean(value);
-    this.#active = value;
-  }
-
-  constructor({ id, actions = [], score = 0, active = false }) {
+  constructor({ id, actions = [], score = 0 }) {
     type.string(id);
     type.strings(actions);
 
     Object.defineProperty(this, 'id', { value: String(id) });
     Object.defineProperty(this, 'actions', { value: new Set(actions) });
     this.score = score;
-    this.active = active;
   }
 
   toJSON() {
@@ -38,7 +26,6 @@ export default class Player {
       id: this.id,
       actions: Array.from(this.actions),
       score: this.score,
-      active: this.active,
     };
   }
 }
--- test/Player.test.js
+++ test/Player.test.js
@@ -8,8 +8,6 @@ test('create new', (t) => {
   t.truthy(player.actions instanceof Set);
   t.is(player.actions.size, 0);
   t.is(player.score, 0);
-  t.is(player.active, false);
-
   t.throws(() => new Player());
 });
 
@@ -20,22 +18,17 @@ test('change', (t) => {
   t.throws(() => { player.score = false; });
   t.notThrows(() => { player.score = 5; });
   t.is(player.score, 5);
-  t.throws(() => { player.active = 0; });
-  t.notThrows(() => { player.active = true; });
-  t.is(player.active, true);
 });
 
 test('serialize', (t) => {
   const player = new Player({ id: 'abc' });
   player.actions.add('go');
   player.score = 5;
-  player.active = true;
   const json = player.toJSON();
   t.deepEqual(json, {
     id: 'abc',
     actions: ['go'],
     score: 5,
-    active: true,
   });
 });
 
@@ -43,12 +36,10 @@ test('restore', (t) => {
   const player = new Player({ id: 'abc' });
   player.actions.add('go');
   player.score = 5;
-  player.active = true;
   const json = player.toJSON();
 
   const restoredPlayer = new Player(json);
   t.is(restoredPlayer.id, 'abc');
   t.truthy(restoredPlayer.actions.has('go'));
   t.is(restoredPlayer.score, 5);
-  t.is(restoredPlayer.active, true);
 });
