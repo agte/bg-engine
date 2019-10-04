1b7bdda63b4944f45480d7cb57c98b5982ecc043
Anna Agte <matiasrustagte@gmail.com>
2019-10-04 18:57:58 +0500

    test: split "StringSet" tests into smaller and simplier parts

--- test/StringSet.test.js
+++ test/StringSet.test.js
@@ -2,51 +2,74 @@ import { serial as test } from 'ava';
 import StringSet from '../src/StringSet.js';
 
 test('class', (t) => {
-  t.is(typeof StringSet, 'function');
   t.true(Object.prototype.isPrototypeOf.call(Set, StringSet));
 });
 
-test('constructor: without data', (t) => {
-  t.notThrows(() => new StringSet());
-  t.notThrows(() => new StringSet([]));
+test('constructor: no data', (t) => {
   const set = new StringSet();
   t.is(set.size, 0);
 });
 
-test('constructor: with data', (t) => {
+test('constructor: wrong data', (t) => {
   t.throws(() => new StringSet(null));
   t.throws(() => new StringSet({ 0: 'abc', 1: 'def' }));
-  let set;
-  t.notThrows(() => { set = new StringSet(['abc', 'def']); });
+});
+
+test('constructor: empty data', (t) => {
+  const set = new StringSet([]);
+  t.is(set.size, 0);
+});
+
+test('constructor: non empty data', (t) => {
+  const set = new StringSet(['abc', 'def']);
   t.is(set.size, 2);
   t.true(set.has('abc'));
   t.deepEqual(Array.from(set), ['abc', 'def']);
 });
 
-test('method "has"', (t) => {
+test('method "has": wrong arguments', (t) => {
   const set = new StringSet(['a', 'b', 'c']);
   t.throws(() => set.has(5));
   t.throws(() => set.has(''));
+});
+
+test('method "has": right arguments', (t) => {
+  const set = new StringSet(['a', 'b', 'c']);
   t.false(set.has('f'));
   t.true(set.has('b'));
 });
 
-test('method "add"', (t) => {
+test('method "add": wrong arguments', (t) => {
   const set = new StringSet(['a', 'b', 'c']);
   t.throws(() => set.add(5));
   t.throws(() => set.add(''));
-  t.notThrows(() => set.add('a'));
-  t.notThrows(() => set.add('d'));
+});
+
+test('method "add": right arguments', (t) => {
+  const set = new StringSet(['a', 'b', 'c']);
+  set.add('a');
+  set.add('d');
   t.is(set.size, 4);
   t.deepEqual(Array.from(set), ['a', 'b', 'c', 'd']);
 });
 
-test('method "delete"', (t) => {
+test('method "add": right order', (t) => {
+  const set = new StringSet(['x', 'a', 'b']);
+  set.add('a');
+  set.add('q');
+  t.deepEqual(Array.from(set), ['x', 'a', 'b', 'q']);
+});
+
+test('method "delete": wrong arguments', (t) => {
   const set = new StringSet(['a', 'b', 'c']);
   t.throws(() => set.delete(5));
   t.throws(() => set.delete(''));
-  t.notThrows(() => set.delete('f'));
-  t.notThrows(() => set.delete('a'));
+});
+
+test('method "delete": right arguments', (t) => {
+  const set = new StringSet(['a', 'b', 'c']);
+  set.delete('f');
+  set.delete('a');
   t.is(set.size, 2);
   t.deepEqual(Array.from(set), ['b', 'c']);
 });
